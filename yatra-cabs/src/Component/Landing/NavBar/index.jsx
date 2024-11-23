0dd904ae14570e018600641310f56a41546889c0
mitalrs <mitalsapkale1999@gmail.com>
2024-11-23 18:28:46 +0530

    maked nav bar component responsive

--- yatra-cabs/src/Component/Landing/NavBar/index.jsx
+++ yatra-cabs/src/Component/Landing/NavBar/index.jsx
@@ -1,34 +1,31 @@
-import './style.css'
-import logo from '../../../assets/logo.png'
+import "./style.css";
+import logo from "../../../assets/logo.png";
 import { RiMobileDownloadLine } from "react-icons/ri";
 import { FaRegUserCircle } from "react-icons/fa";
 
-
-
 function Index() {
-
   return (
     <>
-      <div className='nav-bar'>
+      <div className="nav-bar">
         <div>
-        <img className='logo' src={logo} alt="Logo" />
+          <img className="logo" src={logo} alt="Logo" />
         </div>
-        <div className='contact'>
-        +917860663399
+        <div className="contact">
+        <a href="tel:+917860663399">+917860663399</a>
         </div>
-        <div className='right-menu-items'>
-          <div className='item-container'>
-            <RiMobileDownloadLine className='icon'/>
-           <p>Download App</p>
+        <div className="right-menu-items">
+          <div className="item-container download-icon">
+            <RiMobileDownloadLine className="icon" />
+            <p>Download App</p>
           </div>
-          <div className='item-container'>
-            <FaRegUserCircle className='icon'/>
+          <div className="item-container">
+            <FaRegUserCircle className="icon" />
             <p>Hi, UserName</p>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
