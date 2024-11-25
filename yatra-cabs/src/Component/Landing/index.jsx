3af39dacca47cc6cdc1d1c6d9593d3bfc3a336d6
mitalrs <mitalsapkale1999@gmail.com>
2024-11-25 20:45:38 +0530

    responsive tag line

--- yatra-cabs/src/Component/Landing/index.jsx
+++ yatra-cabs/src/Component/Landing/index.jsx
@@ -12,14 +12,17 @@ function Index() {
       <NavBar/> 
       <div className='wrapper-on-bg-img'>
         <div className='bg-image-on-landing'>
+          <div className='dark-image-background'>
             <div className='tag-lines'>
             <div className='tag-line-wrapper'>
-                <p className='tag-line-first-item'>​India's <p className='tag-line-bold tag-line-bold-1'>Leading</p> </p>
-                <p className='tag-line-bold'>One-Way Inter-City</p>
-                <p>Cab Service Provider</p>  
+              <h1 className='tag-line-first-item'>​India's <span className='tag-line-bold'>Leading One-Way Inter-City </span> Cab Service Provider</h1>
+                <p > <p className=' tag-line-bold-1'></p> </p>
+                <p className='tag-line-bold'></p>
+                <p></p>  
             </div>
             </div>
             <ExploreCab/>
+            </div>
         </div>
         <Ad/>
       </div>
