eb210f6b25812919e67880bead3caec78369ba19
mitalrs <mitalsapkale1999@gmail.com>
2024-11-26 18:50:34 +0530

    why cab comp

--- yatra-cabs/src/App.jsx
+++ yatra-cabs/src/App.jsx
@@ -5,7 +5,7 @@ import LocalTaxiServices from './Component/LocalTaxiServices'
 import WhyYatri from './Component/WhyYatri'
 import FareTable from './Component/FareTable'
 import OneWayJourney from './Component/OneWayJourney'
-// import WhyCab from './Component/WhyCab'
+import WhyCab from './Component/WhyCab'
 // import RoadTrip from './Component/RoadTrip'
 // import CarRental from './Component/CarRental'
 // import Footer from './Component/Footer'
@@ -21,8 +21,8 @@ function App() {
       <WhyYatri/>
       <FareTable/>
       <OneWayJourney/>
-      {/* <WhyCab/>
-      <RoadTrip/>
+      <WhyCab/>
+      {/* <RoadTrip/>
       <CarRental/>
       <Footer/> */}
       </div>
--- yatra-cabs/src/Component/WhyYatri/index.jsx
+++ yatra-cabs/src/Component/WhyYatri/index.jsx
@@ -1,82 +1,73 @@
-import './style.css'
-import car from '../../assets/cab.png'
+import "./style.css";
+import car from "../../assets/cab.png";
 import { FiFlag } from "react-icons/fi";
 import { FaUsers } from "react-icons/fa6";
 import { BsPhone } from "react-icons/bs";
 import { LuCircleDollarSign } from "react-icons/lu";
 
-
-
-
-
 function Index() {
-
   return (
     <>
-       <div className="why-yatri">
-      <div className="content-container">
-      <h2 className="section-title mobile-view">
-        WHY <span className="highlight">YATRI</span> ?
-      </h2>
-        <div className="image-container">
-          <img
-            src={car} 
-            alt="Car"
-            className="car-image"
-          />
-        </div>
-        <div className="features-container">
-        <h2 className="section-title desktop-view">
-        WHY <span className="highlight"> YATRI</span> ?
-      </h2>
-          <div className="feature">
-            <FiFlag className="feature-icon"/>
-            <div>
-              <h3 className="feature-title">FAST RESPONSE TIME</h3>
-              <p className="feature-description">
-                Experience The Best Car Rental Service With Driver For Local Or
-                Outstation Trips. Enjoy Prompt Response Times And Seamless Car
-                Hire Service, Ensuring You Get On The Road Quickly.
-              </p>
-            </div>
+      <div className="why-yatri">
+        <div className="content-container">
+          <h2 className="section-title mobile-view">
+            WHY <span className="highlight">YATRI</span> ?
+          </h2>
+          <div className="image-container">
+            <img src={car} alt="Car" className="car-image" />
           </div>
-          <div className="feature">
-            <FaUsers className="feature-icon"/>
-            <div>
-              <h3 className="feature-title">VAST FLEET</h3>
-              <p className="feature-description">
-                We Offer A Wide Range Of Rental Cars Including Sedans, SUVs,
-                MUVs, Premium Sedans, And Tempo Travellers To Meet All Your
-                Needs.
-              </p>
+          <div className="features-container">
+            <h2 className="section-title desktop-view">
+              WHY <span className="highlight"> YATRI</span> ?
+            </h2>
+            <div className="feature">
+              <FiFlag className="feature-icon" />
+              <div>
+                <h3 className="feature-title">FAST RESPONSE TIME</h3>
+                <p className="feature-description">
+                  Experience The Best Car Rental Service With Driver For Local
+                  Or Outstation Trips. Enjoy Prompt Response Times And Seamless
+                  Car Hire Service, Ensuring You Get On The Road Quickly.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="feature">
-            <BsPhone className="feature-icon"/>
-            <div>
-              <h3 className="feature-title">EASY TO ORDER</h3>
-              <p className="feature-description">
-                Easily Book A Cab Online Through Our User-Friendly Website,
-                Mobile Application, Or By Calling Our Customer Support Team.
-              </p>
+            <div className="feature">
+              <FaUsers className="feature-icon" />
+              <div>
+                <h3 className="feature-title">VAST FLEET</h3>
+                <p className="feature-description">
+                  We Offer A Wide Range Of Rental Cars Including Sedans, SUVs,
+                  MUVs, Premium Sedans, And Tempo Travellers To Meet All Your
+                  Needs.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="feature">
-            <LuCircleDollarSign className="feature-icon"/>
-            <div>
-              <h3 className="feature-title">GREAT TARIFFS</h3>
-              <p className="feature-description">
-                Rent A Car At The Lowest Rates! Our Car Hire Tariffs Are Highly
-                Competitive Compared To Other Operators. Book Online Cabs At The
-                Best Prices With Yatri Car Rental.
-              </p>
+            <div className="feature">
+              <BsPhone className="feature-icon" />
+              <div>
+                <h3 className="feature-title">EASY TO ORDER</h3>
+                <p className="feature-description">
+                  Easily Book A Cab Online Through Our User-Friendly Website,
+                  Mobile Application, Or By Calling Our Customer Support Team.
+                </p>
+              </div>
+            </div>
+            <div className="feature">
+              <LuCircleDollarSign className="feature-icon" />
+              <div>
+                <h3 className="feature-title">GREAT TARIFFS</h3>
+                <p className="feature-description">
+                  Rent A Car At The Lowest Rates! Our Car Hire Tariffs Are
+                  Highly Competitive Compared To Other Operators. Book Online
+                  Cabs At The Best Prices With Yatri Car Rental.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
